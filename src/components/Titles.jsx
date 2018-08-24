7e7bc12e1bb4847b6a7fd1efc20d6bd3396dad02
Afnan Rehman <afnanrehman@192.168.1.44>
2018-08-23 23:20:50 -0400

    First commit

---
+++ src/components/Titles.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+const Titles = () => {
+  return (
+    <div>
+      <h1 className="title-container__title">Weather Finder!</h1>
+      <p className="title-container__subtitle">
+        We tell you the weather of the location of your liking!
+      </p>
+    </div>
+  );
+};
+
+export default Titles;
